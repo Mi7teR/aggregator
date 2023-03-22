4d4d1b30301c80ec96ff39d431e522425e65c528
Olzhas Akhmetkhan <olzhas.akhmetkhan@aitudala.kz>
2023-03-22 16:12:40 +0600

    added tests for TaskMethod entity

--- internal/task/entity/entity_test/task_method_test.go
+++ internal/task/entity/entity_test/task_method_test.go
@@ -1,6 +1,7 @@
 package entity_test
 
 import (
+	"net/http"
 	"reflect"
 	"testing"
 
@@ -14,7 +15,60 @@ func TestTaskMethod_MarshalJSON(t *testing.T) {
 		want    []byte
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"marshal method GET",
+			entity.MethodGet,
+			[]byte(`"GET"`),
+			false,
+		},
+		{
+			"marshal method HEAD",
+			entity.MethodHead,
+			[]byte(`"HEAD"`),
+			false,
+		},
+		{
+			"marshal method POST",
+			entity.MethodPost,
+			[]byte(`"POST"`),
+			false,
+		},
+		{
+			"marshal method PUT",
+			entity.MethodPut,
+			[]byte(`"PUT"`),
+			false,
+		},
+		{
+			"marshal method PATCH",
+			entity.MethodPatch,
+			[]byte(`"PATCH"`),
+			false,
+		},
+		{
+			"marshal method DELETE",
+			entity.MethodDelete,
+			[]byte(`"DELETE"`),
+			false,
+		},
+		{
+			"marshal method CONNECT",
+			entity.MethodConnect,
+			[]byte(`"CONNECT"`),
+			false,
+		},
+		{
+			"marshal method OPTIONS",
+			entity.MethodOptions,
+			[]byte(`"OPTIONS"`),
+			false,
+		},
+		{
+			"marshal method TRACE",
+			entity.MethodTrace,
+			[]byte(`"TRACE"`),
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,7 +90,51 @@ func TestTaskMethod_String(t *testing.T) {
 		t    entity.TaskMethod
 		want string
 	}{
-		// TODO: Add test cases.
+		{
+			"MethodGet String()",
+			entity.MethodGet,
+			http.MethodGet,
+		},
+		{
+			"MethodHead String()",
+			entity.MethodHead,
+			http.MethodHead,
+		},
+		{
+			"MethodPost String()",
+			entity.MethodPost,
+			http.MethodPost,
+		},
+		{
+			"MethodPut String()",
+			entity.MethodPut,
+			http.MethodPut,
+		},
+		{
+			"MethodPatch String()",
+			entity.MethodPatch,
+			http.MethodPatch,
+		},
+		{
+			"MethodDelete String()",
+			entity.MethodDelete,
+			http.MethodDelete,
+		},
+		{
+			"MethodConnect String()",
+			entity.MethodConnect,
+			http.MethodConnect,
+		},
+		{
+			"MethodOptions String()",
+			entity.MethodOptions,
+			http.MethodOptions,
+		},
+		{
+			"MethodTrace String()",
+			entity.MethodTrace,
+			http.MethodTrace,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -57,7 +155,102 @@ func TestTaskMethod_UnmarshalJSON(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"MethodGet UnmarshalJSON()",
+			entity.MethodGet,
+			args{
+				i: []byte(`"GET"`),
+			},
+			false,
+		},
+		{
+			"MethodHead UnmarshalJSON()",
+			entity.MethodHead,
+			args{
+				i: []byte(`"HEAD"`),
+			},
+			false,
+		},
+		{
+			"MethodPost UnmarshalJSON()",
+			entity.MethodPost,
+			args{
+				i: []byte(`"POST"`),
+			},
+			false,
+		},
+		{
+			"MethodPut UnmarshalJSON()",
+			entity.MethodPut,
+			args{
+				i: []byte(`"PUT"`),
+			},
+			false,
+		},
+		{
+			"MethodPatch UnmarshalJSON()",
+			entity.MethodPatch,
+			args{
+				i: []byte(`"PATCH"`),
+			},
+			false,
+		},
+		{
+			"MethodDelete UnmarshalJSON()",
+			entity.MethodDelete,
+			args{
+				i: []byte(`"DELETE"`),
+			},
+			false,
+		},
+		{
+			"MethodConnect UnmarshalJSON()",
+			entity.MethodConnect,
+			args{
+				i: []byte(`"CONNECT"`),
+			},
+			false,
+		},
+		{
+			"MethodOptions UnmarshallJSON()",
+			entity.MethodOptions,
+			args{
+				i: []byte(`"OPTIONS"`),
+			},
+			false,
+		},
+		{
+			"MethodTrace UnmarshallJSON()",
+			entity.MethodTrace,
+			args{
+				i: []byte(`"TRACE"`),
+			},
+			false,
+		},
+		{
+			"MethodGet UnmarshallJSON() prefix not found error",
+			entity.MethodGet,
+			args{
+				i: []byte(`GET"`),
+			},
+			true,
+		},
+		{
+			"MethodGet UnmarshallJSON() suffix not found error",
+			entity.MethodGet,
+			args{
+				i: []byte(`"GET`),
+			},
+			true,
+		},
+		{
+			"undefined method UnmarshallJSON()",
+			entity.MethodGet,
+			args{
+				i: []byte(`"undefined"`),
+			},
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
